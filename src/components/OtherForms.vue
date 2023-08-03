a3af05789002a602b17e46848bc882cbe54d7e42
Artem <artem.kulchitsky@gmail.com>
2023-08-03 21:50:57 +0200

    Add other posts component

---
+++ src/components/OtherForms.vue
@@ -0,0 +1,44 @@
+<template>
+	<section class="main__other other">
+		<h3>Inne ankiety</h3>
+		<article class="other__form" v-for="form in otherForms" :key="form">
+			<h4>№ {{ form }}</h4>
+			<router-link :to="{ name: 'form', params: { id: form } }">Zobacz</router-link>
+		</article>
+	</section>
+</template>
+
+<script setup>
+const otherForms = [1, 2, 3]
+</script>
+
+<style lang="scss" scoped>
+.other {
+	margin: 30px auto;
+	max-width: 400px;
+	background: #242424;
+	border-radius: 8px;
+	padding: 30px;
+	h3 {
+		font-size: 22px;
+		text-align: center;
+		margin-bottom: 10px;
+	}
+	&__form {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding: 10px;
+		&:not(:last-child) {
+			border-bottom: 1px solid #363636;
+		}
+		a {
+			color: rgb(255, 255, 255, 0.87);
+			transition: all 0.3s ease 0s;
+			&:hover {
+				color: #42b883;
+			}
+		}
+	}
+}
+</style>
--- src/views/ResultPage.vue
+++ src/views/ResultPage.vue
@@ -1,19 +1,13 @@
 <template>
 	<SuccessMessage />
-	<section class="main__other other">
-		<h3>Inne ankiety</h3>
-		<article class="other__form" v-for="form in otherForms" :key="form">
-			<h4>№ {{ form }}</h4>
-			<router-link :to="{ name: 'form', params: { id: form } }">Zobacz</router-link>
-		</article>
-	</section>
+	<OtherForms />
 </template>
 
 <script setup>
+import OtherForms from '@/components/OtherForms.vue';
 import SuccessMessage from '@/components/SuccessMessage.vue';
 import { onMounted } from 'vue';
 import { useRouter } from 'vue-router'
-const otherForms = [1, 2, 3]
 const router = useRouter()
 
 onMounted(() => {
@@ -22,34 +16,3 @@ onMounted(() => {
 	}
 })
 </script>
-
-<style lang="scss" scoped>
-.other {
-	margin: 30px auto;
-	max-width: 400px;
-	background: #242424;
-	border-radius: 8px;
-	padding: 30px;
-	h3 {
-		font-size: 22px;
-		text-align: center;
-		margin-bottom: 10px;
-	}
-	&__form {
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		padding: 10px;
-		&:not(:last-child) {
-			border-bottom: 1px solid #363636;
-		}
-		a {
-			color: rgb(255, 255, 255, 0.87);
-			transition: all 0.3s ease 0s;
-			&:hover {
-				color: #42b883;
-			}
-		}
-	}
-}
-</style>
